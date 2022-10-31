ca16d58b8266e802637a04d5f4df60413e119767
9SMAK <barabanov.sam@gmail.com>
2022-10-31 16:33:06 +0300

    Fix

--- src/api/utils/router.py
+++ src/api/utils/router.py
@@ -7,13 +7,13 @@ from src.database.schemas import User, Skill, IdeaTag
 router = APIRouter(prefix="/utils", tags=["Utils"])
 
 
-@router.get('/get_skills', response_model=Skill)
+@router.get('/get_skills')
 async def get_skills():
     result = await SKILL.get_all()
     return result
 
 
-@router.get('/get_idea_tags', response_model=IdeaTag)
+@router.get('/get_idea_tags')
 async def get_idea_tags():
     result = await IDEATAG.get_all()
     return result
