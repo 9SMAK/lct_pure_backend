9c009815441835b76ccd9d99528f3947cd07332b
9SMAK <barabanov.sam@gmail.com>
2022-11-02 22:49:29 +0500

    Remove smth

--- src/api/admin/router.py
+++ src/api/admin/router.py
@@ -1,12 +1,12 @@
 from fastapi import APIRouter
 
 from src.api.schemas import OkResponse
-from src.database.repositories import USER, IDEA, SKILL, IDEATAG
-from src.database.schemas import User
+from src.database.repositories import IDEA, SKILL, IDEATAG
 
 router = APIRouter(prefix="/admin", tags=["Admin"])
 
 
+# TODO check if admin
 @router.post('/approve_idea', response_model=OkResponse)
 async def approve_idea(id: int) -> OkResponse:
     await IDEA.approve_idea(idea_id=id)
@@ -23,4 +23,3 @@ async def add_skill(name: str) -> OkResponse:
 async def add_idea_tag(name: str) -> OkResponse:
     await IDEATAG.add(name=name)
     return OkResponse()
-
--- src/api/admin/schemas.py
+++
@@ -1,5 +0,0 @@
-import json
-from typing import List, Dict
-from pydantic import BaseModel
-
-
